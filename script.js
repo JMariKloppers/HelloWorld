21bd7fa905408d5fbdd192cf2de5ea77742ddbce
JMariKloppers <jkloppers08@gmail.com>
2023-05-16 15:47:15 +0200

    Added weekend check function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello world");
+
+const today = "Tuesday";
+
+const weekend = (day) => {
+  if (day === "Friday") {
+    return "WEEKEND";
+  } else {
+    return "WORK BABY";
+  }
+};
+
+console.log(weekend(today)); //check if today is the weekend
